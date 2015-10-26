75b6fc755a17f1a1681d48c3cebafba20b2b9ab3
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-26 11:42:23 -0400

    reset button to hide questions working

--- triviascript.js
+++ triviascript.js
@@ -6,13 +6,29 @@ $(document).ready(function(){
       .attr("id", "trivia")
       .addClass("questions")
     )
+    $( ".start" ).click(function() {
+        $( "p" ).show( "slow" );
+      });
+
+  // $('body').append(
+  //     $('button')
+  //       .attr('type', 'button')
+  //       .addClass("questions")
+  //     )
     // $('.go').on('click', 'img', function(e) {
     //     $(this).width(100).height(100).appendTo('#badgeselect');
     //     $('#modal').modal({'show':true});
     // });
 
-    $( "button" ).click(function() {
-      $( "p" ).show( "slow" );
+
+
+    $('.reset').click(function(){
+        $(':input','p')
+        $('p').hide('fast');
+        // .not(':button, :submit, :reset, :hidden')
+        // .val('')
+        // .removeAttr('checked')
+        // .removeAttr('selected');
     });
 
 })
