903910b554f6102c3d308869380e59911a775226
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-26 10:54:25 -0400

    button to display questions added and funtioning

--- triviascript.js
+++ triviascript.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   console.log('SGo');
 
+  $('container').append(
+    $('<div>')
+      .attr("id", "trivia")
+      .addClass("questions")
+    )
+    // $('.go').on('click', 'img', function(e) {
+    //     $(this).width(100).height(100).appendTo('#badgeselect');
+    //     $('#modal').modal({'show':true});
+    // });
 
+    $( "button" ).click(function() {
+      $( "p" ).show( "slow" );
+    });
 
 })
