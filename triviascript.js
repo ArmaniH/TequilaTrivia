773f3a68695e08e3959adbaa7b62e938d1ca2e7a
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-28 01:47:15 -0400

    Clear still not working and looping has some how broken

--- triviascript.js
+++ triviascript.js
@@ -1,65 +1,53 @@
 $(document).ready(function(){
+
   console.log('SGo');
 
-// Array of questions
-var i = 0;
+// Array of questions and answers
 var tQuestions = {
-  question: ['What plant is Tequila distilled from?'], answers: ['Maguey', 'Agave Americana', 'Agave Azul', 'Agave Attenuata'],
+  question1: ['What plant is Tequila distilled from?'], answers1: ['Maguey', 'Agave Americana', 'Agave Azul', 'Agave Attenuata'],
   question2: ['In which Mexican State is the town of Tequila located?'], answers2: ['Jalisco', 'Chihuahua', 'Durango', 'Oaxaca'],
-  
+  question3: ['What is the traditional way to serve Tequila?'], answers3: ['Lime & Salt', 'Sangrita', 'Orange & Cinnamon', 'Out of Your Navel with High Fives'],
+  question4: ['The Agave plant is also used to make what?'], answers4: ['Lotions', 'Textiles', 'Cosmetics', 'Sweeteners'],
+  question5: ['Tequila was first served out of...'], answers5: ['1 oz. Shot Glass', '2 oz. Shot Glass', 'Bulls Horn', 'Out of Your Navel with High Fives'],
+  question6: ['"Worms" are found in which type of Tequila?'], answers6: ['Ordinario', 'Mezcal', 'Corazon', 'Cabeza'],
+  question7: ['The Blue Agave is closley related to which other plants?'], answers7: ['Lily','Cacti', 'Aloe', 'Asparagus'],
 }
-// var triviaQuestions = ['What plant is Tequila distilled from?',
-//                       'Which Mexican state is the town of Tequila located in?',
-//                       'What is the traditional way to serve Tequila?',
-//                       'The Agave plant is also used to make what?',
-//                       'Tequila was first served out of...',
-//                     ];
+
     console.log(tQuestions)
 
-// Object Array of answer sets
-// var triviaAnswers = {
-//     Answer1: ['Maguey', 'Agave Americana', 'Agave Azul', 'Agave Attenuata'],
-//     Answer2: ['Jalisco', 'Chihuahua', 'Durango', 'Oaxaca'],
-//     Answer3: ['Lime & Salt', 'With Sangrita', 'Orange & Cinnamon', 'Out of Your Navel with High Fives'],
-//     Answer4: ['Lotions', 'Textiles', 'Fuel', 'Sweeteners'],
-//     Answer5: ['1 oz. Shot Glass', '2 oz. Shot Glass', 'Bulls Horn', 'Out of Your Navel with High Fives'],
-//     Answer6: [],
-//     Answer7: [],
-//     Answer8: [],
-//     Answer9: [],
-//     Answer10: [],
-  // }
-    // console.log(answers)
 
 //Clicking start displays first question and 4 potential answers
 
   $('.start').click(function(e) {
     console.log(tQuestions)
     e.preventDefault();
+    $('fieldset').hide('slow');
     $('p').show('slow');
     $('h2').show('slow');
-  })
-$('.q1').html('<h2>'+tQuestions.question+'</h2>')
-    for (i = 0; i < tQuestions.answers.length; ++i) {
-      var answer = tQuestions.answers[i]
+    $('h3').show('slow');
+
+$('.q1').html('<h2>'+tQuestions.question1+'</h2>')
+    for (i = 0; i < tQuestions.answers1.length; ++i) {
+      var answer = tQuestions.answers1[i]
       var btn = $('<button>'+answer+'</button>')
+// Click event to indicate the correct answer was chosen
       if( i == 2 ){
           btn.addClass("correct");
         }
         $(".q1").append(btn)
       }
-// Click event to indicate the correct answer was chosen
+
       $("button").on("click", function(){
           if($(this).hasClass("correct")){
 // Assign a point for each correct answer
-          tQuestions.question = tQuestions.question + 1;
+          tQuestions.question = tQuestions.question1 + 1;
             $('#counter').html(function(i, val) { return +val+1 });
             alert("correct")
 // Click event to indicate the incorrect answer was chosen
           }else{
             $( "button" ).shake;
 
-  }
+  }})
 
 $('.q1').html('<h2>'+tQuestions.question2+'</h2>')
 for (i = 0; i < tQuestions.answers2.length; ++i) {
@@ -77,20 +65,111 @@ for (i = 0; i < tQuestions.answers2.length; ++i) {
         alert("correct")
       }else{
         $( "button" ).shake;
-}
+}})
+
+
+$('.q1').html('<h2>'+tQuestions.question3+'</h2>')
+for (i = 0; i < tQuestions.answers3.length; ++i) {
+  var answer = tQuestions.answers3[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 1 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $( "button" ).shake;
+}})
+
+
+$('.q1').html('<h2>'+tQuestions.question4+'</h2>')
+for (i = 0; i < tQuestions.answers4.length; ++i) {
+  var answer = tQuestions.answers4[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 2 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $( "button" ).shake;
+}})
+
+
+$('.q1').html('<h2>'+tQuestions.question5+'</h2>')
+for (i = 0; i < tQuestions.answers5.length; ++i) {
+  var answer = tQuestions.answers5[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 2 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $( "button" ).shake;
+}})
+
+
+$('.q1').html('<h2>'+tQuestions.question6+'</h2>')
+for (i = 0; i < tQuestions.answers6.length; ++i) {
+  var answer = tQuestions.answers6[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 1 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $( "button" ).shake;
+}})
+
+
+$('.q1').html('<h2>'+tQuestions.question7+'</h2>')
+for (i = 0; i < tQuestions.answers7.length; ++i) {
+  var answer = tQuestions.answers7[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 0 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $(this).effect( "shake" );
+}})
+})
 })
 
 
-})
-
-})
 // Restart button hides question and answers
   $('.reset').click(function(){
     $(':input','p')
     $('p').hide('fast');
     $('h2').hide('fast');
-    })
-
-
+  })
 
 // Click event to refresh question AND answer array after answer is selected
