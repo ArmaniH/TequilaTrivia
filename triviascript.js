434e6340e4063903879ed955d3442438fd48c716
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-27 20:09:43 -0400

    arrays looping and displaying properly. Points are tallying properly. hide, shake, show still not working properly

--- triviascript.js
+++ triviascript.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   console.log('SGo');
 
 // Array of questions
+var i = 0;
 var tQuestions = {
-  question: 'What plant is Tequila distilled from?', answers: [ 'Maguey', 'Agave Americana', 'Agave Azul', 'Agave Attenuata'],
-  question2: 'In which Mexican State is the town of Tequila located?', answers2: ['Jalisco', 'Chihuahua', 'Durango', 'Oaxaca'],
+  question: ['What plant is Tequila distilled from?'], answers: ['Maguey', 'Agave Americana', 'Agave Azul', 'Agave Attenuata'],
+  question2: ['In which Mexican State is the town of Tequila located?'], answers2: ['Jalisco', 'Chihuahua', 'Durango', 'Oaxaca'],
+  
 }
 // var triviaQuestions = ['What plant is Tequila distilled from?',
 //                       'Which Mexican state is the town of Tequila located in?',
@@ -37,7 +39,7 @@ var tQuestions = {
     $('p').show('slow');
     $('h2').show('slow');
   })
-    $('.q1').html('<h2>'+tQuestions.question+'</h2>')
+$('.q1').html('<h2>'+tQuestions.question+'</h2>')
     for (i = 0; i < tQuestions.answers.length; ++i) {
       var answer = tQuestions.answers[i]
       var btn = $('<button>'+answer+'</button>')
@@ -46,38 +48,40 @@ var tQuestions = {
         }
         $(".q1").append(btn)
       }
-
+// Click event to indicate the correct answer was chosen
       $("button").on("click", function(){
           if($(this).hasClass("correct")){
-            // $("update").(function() {
+// Assign a point for each correct answer
+          tQuestions.question = tQuestions.question + 1;
             $('#counter').html(function(i, val) { return +val+1 });
-
-
             alert("correct")
+// Click event to indicate the incorrect answer was chosen
           }else{
             $( "button" ).shake;
 
   }
 
+$('.q1').html('<h2>'+tQuestions.question2+'</h2>')
+for (i = 0; i < tQuestions.answers2.length; ++i) {
+  var answer = tQuestions.answers2[i]
+  var btn = $('<button>'+answer+'</button>')
+  if( i == 0 ){
+      btn.addClass("correct");
+    }
+    $(".q1").append(btn)
+  }
+  $("button").on("click", function(){
+    tQuestions.question = tQuestions.question + 1;
+      $('#counter').html(function(i, val) { return +val+1 });
+      if($(this).hasClass("correct")){
+        alert("correct")
+      }else{
+        $( "button" ).shake;
+}
+})
 
-// $('.q1').html('<h2>'+tQuestions.question2+'</h2>')
-// for (i = 0; i < tQuestions.answers2.length; ++i) {
-//   var answer = tQuestions.answers2[i]
-//   var btn = $('<button>'+answer+'</button>')
-//   if( i == 0 ){
-//       btn.addClass("correct");
-//     }
-//     $(".q1").append(btn)
-//   }
-//
-//   $("button").on("click", function(){
-//       if($(this).hasClass("correct")){
-//         alert("correct")
-//       }else{
-//         $( "button" ).shake;
-// }
-// })
 
+})
 
 })
 // Restart button hides question and answers
@@ -85,13 +89,8 @@ var tQuestions = {
     $(':input','p')
     $('p').hide('fast');
     $('h2').hide('fast');
-
     })
-})
-// Click event to indicate the correct answer was chosen
 
-// Click event to indicate the incorrect answer was chosen
+
 
 // Click event to refresh question AND answer array after answer is selected
-
-// Assign a point for each correct answer
